2ea61c831b654d0e0ac0692fbbab0ec17a878eb4
khalilsayhi <khalil.sayhi1@esprit.tn>
2020-11-25 17:41:01 +0100

    before services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FooComponent} from './foo/foo.component';
+import {HomeComponent} from './home/home.component';
+import {DishesComponent} from './dishes/dishes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/Home', pathMatch: 'full'},
+  {path: 'Home', component : HomeComponent},
+  {path: 'Dishes', component : DishesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModalComponent } from './login-modal/login-modal.component';
+import { FooComponent } from './foo/foo.component';
+import { HomeComponent } from './home/home.component';
+import { DishesComponent } from './dishes/dishes.component';
+import { DishComponent } from './dish/dish.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginModalComponent,
+    FooComponent,
+    HomeComponent,
+    DishesComponent,
+    DishComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
